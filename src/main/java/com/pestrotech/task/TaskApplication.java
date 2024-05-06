b4be366210ac3506caf170fc445e1ad7fff59bd0
avishdhirawat <avishdhirawat0@gmail.com>
2024-05-06 19:42:25 +0530

    FirebaseConfig and application.properties changes

--- src/main/java/com/pestrotech/task/TaskApplication.java
+++ src/main/java/com/pestrotech/task/TaskApplication.java
@@ -2,12 +2,14 @@ package com.pestrotech.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.pestrotech.task", "com.pestrotech.task.repository"})
 public class TaskApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplication.class, args);
+    }
 
 }
--- src/main/java/com/pestrotech/task/repository/TaskRepository.java
+++ src/main/java/com/pestrotech/task/repository/TaskRepository.java
@@ -1,8 +1,11 @@
 package com.pestrotech.task.repository;
 
 import com.pestrotech.task.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+@Repository
+public interface TaskRepository extends CrudRepository<Task, Long> {
 }
 
