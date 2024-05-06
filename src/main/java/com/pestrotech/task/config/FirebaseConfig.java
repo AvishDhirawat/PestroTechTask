44b7baf1ee00a19054eacd8a133e2b7ec23c4e9a
avishdhirawat <avishdhirawat0@gmail.com>
2024-05-06 17:53:08 +0530

    FirebaseConfig and application.properties changes

--- src/main/java/com/pestrotech/task/config/FirebaseConfig.java
+++ src/main/java/com/pestrotech/task/config/FirebaseConfig.java
@@ -14,23 +14,26 @@ public class FirebaseConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("D:\\Downloads\\pesto-tech-assessment-firebase-adminsdk-xv9wv-3a9dc8ec49.json");
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
-
-//        FileInputStream serviceAccount =
-//                new FileInputStream("path/to/serviceAccountKey.json");
-
+//        FileInputStream serviceAccount = new FileInputStream("D:\\Downloads\\pesto-tech-assessment-firebase-adminsdk-xv9wv-3a9dc8ec49.json");
+//
 //        FirebaseOptions options = new FirebaseOptions.Builder()
 //                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                .setDatabaseUrl("https://pesto-tech-assessment-default-rtdb.asia-southeast1.firebasedatabase.app")
 //                .build();
+        if (FirebaseApp.getApps().isEmpty()) {
+            FileInputStream serviceAccount =
+                    new FileInputStream("D:/Downloads/pesto-tech-assessment-firebase-adminsdk-xv9wv-3a9dc8ec49.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://pesto-tech-assessment-default-rtdb.asia-southeast1.firebasedatabase.app")
+                    .build();
 //
 //        FirebaseApp.initializeApp(options);
 
 
-        return FirebaseApp.initializeApp(options);
+            return FirebaseApp.initializeApp(options);
+        } else {
+            return FirebaseApp.getInstance();
+        }
     }
 }
